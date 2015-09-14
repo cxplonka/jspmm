52cb35138e619531a3c14251a3a978d64793cc9a
Christian Plonka <cplonka81@gmail.com>
2015-09-14 15:18:00 +0200

    Fix missing file in benchmark.

--- jspmm-annotation/test/com/jspmm/Benchmark_CRS_CCS_Test.java
+++ jspmm-annotation/test/com/jspmm/Benchmark_CRS_CCS_Test.java
@@ -23,14 +23,8 @@
  */
 package com.jspmm;
 
-import com.jspmm.matrix.AbstractMatrix;
 import com.jspmm.matrix.CCSMatrix;
 import com.jspmm.matrix.CRSMatrix;
-import com.jspmm.matrix.MutableCOOMatrix;
-import com.jspmm.util.CCSStreamMatrix;
-import com.jspmm.util.CRSStreamMatrix;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 /**
  *
@@ -43,21 +37,21 @@ public class Benchmark_CRS_CCS_Test {
 
     static SpMM service;
 
-    @BeforeClass
-    public static void setUpClass() {
-        service = new StreamSpMM();
-
-        m0 = CRSStreamMatrix.readCRSMatrix("d:/crs_sentences.mat");
-        m1 = CCSStreamMatrix.readCCSMatrix("d:/ccs_term.mat");
-    }
-
-    @Test
-    public void testSpMM_m1() {
-        long t0 = System.currentTimeMillis();
-        AbstractMatrix m = service.multiply(m0, m1, MutableCOOMatrix.class);
-        long t1 = System.currentTimeMillis();
-        System.out.println((t1 - t0) + "ms");
-    }
+//    @BeforeClass
+//    public static void setUpClass() {
+//        service = new StreamSpMM();
+//
+//        m0 = CRSStreamMatrix.readCRSMatrix("d:/crs_sentences.mat");
+//        m1 = CCSStreamMatrix.readCCSMatrix("d:/ccs_term.mat");
+//    }
+//
+//    @Test
+//    public void testSpMM_m1() {
+//        long t0 = System.currentTimeMillis();
+//        AbstractMatrix m = service.multiply(m0, m1, MutableCOOMatrix.class);
+//        long t1 = System.currentTimeMillis();
+//        System.out.println((t1 - t0) + "ms");
+//    }
 
     public static void main(String[] arg) {
 //        Util.createCRSMatrix("d:/crs_sentences.mat", 100, 19000, 0.03);
